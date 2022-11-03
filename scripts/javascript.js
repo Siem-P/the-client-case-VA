1889fb38a2e864e652c8307dbd1738c91bb2a313
Siem-P <siemp2003@gmail.com>
2022-11-03 13:29:24 +0100

    Update Footer

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,16 +1,23 @@
-const navButtons = document.querySelectorAll('.nav-button');
-const cardsContent = document.querySelectorAll('.card-heading');
+const navButtons = document.querySelectorAll(".nav-button");
+const cardsContent = document.querySelectorAll(".card-heading");
 
 console.log(navButtons.length);
 console.log(cardsContent.length);
 
-navButtons.forEach(item => {
-    item.addEventListener('click', () => {
-        const id = item.dataset.id
-        const active = document.getElementById(id)
+navButtons.forEach((item) => {
+  item.addEventListener("click", () => {
+    const id = item.dataset.id;
+    const active = document.getElementById(id);
 
-        cardsContent.forEach(content => content.classList.add('hide-show'))
-        active.classList.remove('hide-show')
+    cardsContent.forEach((content) => content.classList.add("hide-show"));
+    active.classList.remove("hide-show");
+  });
+});
 
-    })
-})
+    const date = new Date();
+    const datum = date.getFullYear();
+    const datumString = "© " + datum;
+    document.getElementById("copyright-year").innerHTML = datumString;
+    // console.log(datum);
+
+
